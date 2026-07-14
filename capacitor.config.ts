533e17877386dba581fd1ab66dd35c1cfbff5eed
yousifa298984-collab <yousif.a298984@gmail.com>
2026-07-14 09:59:39 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nesaj.adhkar',
+  appName: 'أذكار الصباح والمساء',
+  webDir: 'www'
+};
+
+export default config;
